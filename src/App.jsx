11c0aabcf5e07e4ffb93f05f60de387c8e42be82
genshinw <554659766@qq.com>
2023-05-10 15:17:12 +0800

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,9 @@ function App() {
       <h1>React Pitfall</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Go to learn pitfall
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
